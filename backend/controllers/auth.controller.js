f3242a4a7c6f385e83f7898d0585ed8c1a18e247
jeffmikhail <mikhailcruz78@gmail.com>
2024-03-03 00:01:39 +0800

    login and logout endpoints

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -52,10 +52,38 @@ export const signup = async (req, res) => {
 	}
 };
 
-export const login = (req, res) => {
-	res.send("Login user");
+export const login = async (req, res) => {
+	const { username, password } = req.body;
+
+	try {
+		const user = await User.findOne({ username });
+		const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+		if (!user || !isPasswordCorrect) {
+			return res.status(400).json({ error: `Invalid credentials` });
+		}
+
+		generateTokenAndSetCookie(user._id, res);
+
+		res.status(200).json({
+			_id: user._id,
+			fullName: user.fullName,
+			username: user.username,
+			profilePic: user.profilePic,
+		});
+	} catch (error) {
+		console.log(`Error in login controller: ${error.message}`);
+		res.status(500).json({ error: `Internal Server Error` });
+	}
 };
 
 export const logout = (req, res) => {
-	res.send("Logout user");
+	try {
+		res.cookie("jwt", "", { maxAge: 0 });
+
+		res.status(200).json({ message: `Logged out successfully` });
+	} catch (error) {
+		console.log(`Error in logout controller: ${error.message}`);
+		res.status(500).json({ error: `Internal Server Error` });
+	}
 };
