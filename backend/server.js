d04eca1fd7f464510b43fedaa110a3dc9a982a26
jeffmikhail <mikhailcruz78@gmail.com>
2024-03-11 22:27:15 +0800

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
+import path from "path";
 import express from "express";
 import colors from "@colors/colors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
+const __dirname = path.resolve();
 dotenv.config();
 
 import authRoutes from "./routes/auth.routes.js";
@@ -22,6 +24,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
 	connectToMongoDB();
